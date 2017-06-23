08cf63d14e2f01a7e6b951ed53cac2d5c4a9c490
milesaylward <miles.aylward@gmail.com>
2017-06-23 09:59:47 -0400

    button fix

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 $(function() {
   const carousel = document.querySelector('my-carousel');
-  var play = setInterval(_ => carousel.next(), 4000);
+  var play = setInterval(_ => carousel.next(), 6000);
 
   $('#stop').on('click', () => {
     clearInterval(play);
+    $('#start').attr('disabled', 'disabled');
+    $('.warning').addClass('visible');
   });
 });
